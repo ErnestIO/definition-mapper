82e05dbcf62b02ef9872d305156042e8e8915217
Adrià Cidre <adria.cidre@gmail.com>
2017-04-26 13:52:28 +0200

    Allow azure providers

--- libmapper/providers/azure/components/virtualmachine.go
+++ libmapper/providers/azure/components/virtualmachine.go
@@ -7,7 +7,7 @@ package components
 import (
 	"github.com/ernestio/ernestprovider/event"
 	"github.com/ernestio/ernestprovider/providers/azure/virtualmachine"
-	"github.com/r3labs/graph"
+	graph "gopkg.in/r3labs/graph.v2"
 )
 
 // VirtualMachine : A resource group a container that holds
