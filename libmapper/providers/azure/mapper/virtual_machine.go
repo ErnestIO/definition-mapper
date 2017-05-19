c163d63674a46f88843b9ae022ea9975559da816
Tom Bevan <tom.bevan@r3labs.io>
2017-05-19 12:02:18 +0100

    fixed optional values with virtual_machine

--- libmapper/providers/azure/mapper/virtual_machine.go
+++ libmapper/providers/azure/mapper/virtual_machine.go
@@ -43,14 +43,18 @@ func MapVirtualMachines(d *definition.Definition) (vms []*components.VirtualMach
 				cvm.StorageOSDisk.OSType = vm.StorageOSDisk.OSType
 				cvm.StorageOSDisk.CreateOption = vm.StorageOSDisk.CreateOption
 				cvm.StorageOSDisk.ImageURI = vm.StorageOSDisk.ImageURI
-				cvm.StorageOSDisk.VhdURI = fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s.vhd", vm.StorageOSDisk.StorageAccount, vm.StorageOSDisk.StorageContainer, vm.StorageOSDisk.Name+"-"+strconv.Itoa(i))
+				if vm.StorageOSDisk.StorageAccount != "" && vm.StorageOSDisk.StorageContainer != "" {
+					cvm.StorageOSDisk.VhdURI = fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s.vhd", vm.StorageOSDisk.StorageAccount, vm.StorageOSDisk.StorageContainer, vm.StorageOSDisk.Name+"-"+strconv.Itoa(i))
+				}
 				cvm.StorageOSDisk.StorageAccount = vm.StorageOSDisk.StorageAccount
 				cvm.StorageOSDisk.StorageContainer = vm.StorageOSDisk.StorageContainer
 
 				cvm.StorageDataDisk.Name = vm.StorageDataDisk.Name
 				cvm.StorageDataDisk.Size = vm.StorageDataDisk.DiskSizeGB
 				cvm.StorageDataDisk.CreateOption = vm.StorageDataDisk.CreateOption
-				cvm.StorageDataDisk.VhdURI = fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s.vhd", vm.StorageDataDisk.StorageAccount, vm.StorageDataDisk.StorageContainer, vm.StorageDataDisk.Name+"-"+strconv.Itoa(i))
+				if vm.StorageDataDisk.StorageAccount != "" && vm.StorageDataDisk.StorageContainer != "" {
+					cvm.StorageDataDisk.VhdURI = fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s.vhd", vm.StorageDataDisk.StorageAccount, vm.StorageDataDisk.StorageContainer, vm.StorageDataDisk.Name+"-"+strconv.Itoa(i))
+				}
 				cvm.StorageDataDisk.StorageAccount = vm.StorageDataDisk.StorageAccount
 				cvm.StorageDataDisk.StorageContainer = vm.StorageDataDisk.StorageContainer
 
@@ -70,7 +74,9 @@ func MapVirtualMachines(d *definition.Definition) (vms []*components.VirtualMach
 				cvm.Plan.Product = vm.Plan.Product
 				cvm.Plan.Publisher = vm.Plan.Publisher
 
-				cvm.OSProfile.ComputerName = vm.OSProfile.ComputerName + "-" + strconv.Itoa(i)
+				if vm.OSProfile.ComputerName != "" {
+					cvm.OSProfile.ComputerName = vm.OSProfile.ComputerName + "-" + strconv.Itoa(i)
+				}
 
 				cvm.OSProfileLinuxConfig.SSHKeys = mapSSHKeys(vm.Authentication.SSHKeys)
 				cvm.OSProfileLinuxConfig.DisablePasswordAuthentication = vm.Authentication.DisablePasswordAuthentication
