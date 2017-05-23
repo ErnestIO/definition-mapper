73a545f489bb4b3b62acc0acdf0dabeccd443656
Adrià Cidre <adria.cidre@gmail.com>
2017-05-23 13:31:29 +0200

    Fixes #ernest/507 : enable_ip_forwarding as bool

--- libmapper/providers/azure/definition/network_interface.go
+++ libmapper/providers/azure/definition/network_interface.go
@@ -10,7 +10,7 @@ type NetworkInterface struct {
 	Name                 string            `json:"name" yaml:"name"`
 	SecurityGroup        string            `json:"security_group" yaml:"security_group"`
 	InternalDNSNameLabel string            `json:"internal_dns_name_label" yaml:"internal_dns_name_label"`
-	EnableIPForwarding   string            `json:"enable_ip_forwarding" yaml:"enable_ip_forwarding"`
+	EnableIPForwarding   bool              `json:"enable_ip_forwarding" yaml:"enable_ip_forwarding"`
 	DNSServers           []string          `json:"dns_servers" yaml:"dns_servers"`
 	IPConfigurations     []IPConfiguration `json:"ip_configurations" yaml:"ip_configurations"`
 	Tags                 map[string]string `json:"tags" yaml:"tags"`
