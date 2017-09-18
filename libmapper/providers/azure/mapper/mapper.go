0e2bbf6e3026a2d0f4976823ff34a64100efa0a2
Tom Bevan <tom.bevan@r3labs.io>
2017-09-18 14:33:46 +0100

    removing import for managed disks

--- libmapper/providers/azure/mapper/mapper.go
+++ libmapper/providers/azure/mapper/mapper.go
@@ -16,7 +16,7 @@ import (
 )
 
 // SUPPORTEDCOMPONENTS represents all component types supported by ernest
-var SUPPORTEDCOMPONENTS = []string{"network_interface", "public_ip", "resource_group", "security_group", "sql_firewall_rule", "sql_database", "sql_server", "storage_account", "storage_container", "subnet", "virtual_machine", "virtual_network", "lb", "availability_set", "managed_disk"}
+var SUPPORTEDCOMPONENTS = []string{"network_interface", "public_ip", "resource_group", "security_group", "sql_firewall_rule", "sql_database", "sql_server", "storage_account", "storage_container", "subnet", "virtual_machine", "virtual_network", "lb", "availability_set"}
 
 // Mapper : implements the generic mapper structure
 type Mapper struct{}
